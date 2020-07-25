2e836d9ec786f92eb8a6e0ed0ab7c096c517d8cf
Sammy Sloan <smileysammy3@gmail.com>
2020-07-25 17:50:08 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
+// import dependencies 
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
+// setup the express app 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+app.use(morgan("dev"));
 
+// setup express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// mongo db
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
+// starts the server
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
