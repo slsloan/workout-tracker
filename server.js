23be26d2157b702a3cce2d62de413ae0b1870bde
Sammy Sloan <smileysammy3@gmail.com>
2020-07-26 21:01:48 -0600

    updated routes and fixed some bugs

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-// import dependencies 
+// import dependencies
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-// setup the express app 
+// setup express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
-// setup express app to handle data parsing
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-// mongo db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+// mongo database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false
-});
+})
 
-// routes
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));
+// imported routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// starts the server
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+// Starts the server to begin listening
+app.listen(PORT, function () {
+    console.log(`App listening on Port ${PORT}!`);
 });
